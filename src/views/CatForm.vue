d99e86b3e8ebf82649c8f434a98d705fb7e21cbd
abon <antoine.bon@atos.net>
2020-11-24 22:54:01 +0100

    WIP : form

--- src/views/CatForm.vue
+++ src/views/CatForm.vue
@@ -3,49 +3,62 @@
 <div>
   <Header></Header>
     <div class="form">
-      
-
-          <section class="container">
             <h2>Cat card form</h2>
             <hr />
-            <h3 id="formError" class="text-center text-danger d-none"></h3>
-            <form id="cardForm" novalidate>
-              <div class="form-group row">
-                <label for="cardTitle" class="col-sm-2 col-form-label">Card title</label>
-                <input type="text" required class="form-control col-sm-10" id="cardTitle" placeholder="Card title">
-                <div class="invalid-feedback offset-md-2 col-sm-10">
-                  That field is required. Please provide a value.
-                </div>
-              </div>
-              <div class="form-group row">
-                <label for="cardImage" class="col-sm-2 col-form-label">Card image URL</label>
-                <input type="url" required class="form-control col-sm-10" id="cardImage" placeholder="Card image URL">
-                <div class="invalid-feedback offset-md-2 col-sm-10">
-                  That field is required and attempt an URL as value. Please provide a value that respect URL format.
-                </div>
-              </div>
-              <div class="form-group row">
-                <label for="cardDescription" class="col-sm-2 col-form-label">Card description</label>
-                <textarea class="form-control col-sm-10" id="cardDescription"></textarea>
-              </div>
-              <input type="hidden" id="cardId">
-              <button class="btn btn-danger">Delete</button>
-              <button type="submit" class="btn btn-primary">Save</button>
-            </form>
-          </section>
+      
 
-        <b-form>
-          <b-form-group id="input-group-2" label="Card Title:" label-for="input-2">
-            <b-form-input
-              id="input-2"
+                  <b-container fluid>
+  <b-row class="my-1">
+    <b-col sm="3">
+      <label for="input-1">Card title</label>
+    </b-col>
+    <b-col sm="9">
+      <b-form-input
+              id="input-1"
               v-model="form.title"
               required
-              placeholder="Enter title"
-              :state="isTitleValid"
-              :invalid-feedback="invalidFeedback"
+              placeholder="Card title"
+              :state="this.form.title.length > 0"
+
             ></b-form-input>
-          </b-form-group>
-        </b-form>
+    </b-col>
+  </b-row>
+
+  <b-row class="my-1">
+    <b-col sm="3">
+      <label for="input-2">Card image URL</label>
+    </b-col>
+    <b-col sm="9">
+
+      <b-form-input
+              id="input-2"
+              v-model="form.url"
+              required
+              placeholder="Card image URL"
+              :state="isUrlValid"
+
+            ></b-form-input>
+
+    </b-col>
+  </b-row>
+
+  <b-row class="my-1">
+    <b-col sm="3">
+      <label for="input-3">Card description</label>
+    </b-col>
+    <b-col sm="9">
+
+      <b-form-textarea
+              id="input-3"
+              v-model="form.description"
+            ></b-form-textarea>
+
+    </b-col>
+  </b-row>
+</b-container>
+
+    <button type="submit" class="btn btn-primary">Save</button>
+
 
     </div>
   <Footer></Footer>
@@ -76,24 +89,19 @@ export default {
     }
   },
   computed: {
-      // validation() {
-      //   return this.form.title.length > 0
-      // }
-
-      isTitleValid() {
-        console.log('COUCOU',this.form.title.length )
-        if(this.form.title.length > 0) {
+    isTitleValid() {
+        if((this as any).form.title.length > 0) {
           return true;
         }
         return false;
       },
-
-      invalidFeedback() {
-        if (this.form.title.length > 0) {
-          return 'Enter at least 1 characters.'
+    isUrlValid() {
+      const url = (this as any).form.title;
+      if(url.length > 0 && url.substring(0, 7) === "http://") {
+          return true;
         }
-        return 'Please enter something.'
-      }
+        return false;
+    }
   }
 };
 </script>
