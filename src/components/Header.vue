495fa5163b54a8e06cc885e55b26c292774f68cc
abon <antoine.bon@atos.net>
2020-11-24 23:25:18 +0100

    Add back button

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,7 @@
 <template>
   <header class="bg-primary">
+    {{getUrl}}
+    <a v-if="!isRoot" v-bind:style="styleObject" href="/" class="btn btn-primary back-btn"><i class="fas fa-caret-left mr-2"></i>Back</a>
     <h1 class="text-white text-center p-3">Cat card app</h1>
   </header>
 </template>
@@ -9,7 +11,22 @@ import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import Vue from "vue";
 
-export default Vue.extend({
-  name: "Header"
-});
+export default {
+  name: "Header",
+
+  data() {
+    return {
+      styleObject: {
+        position: 'absolute',
+        top: '20px',
+        left: '20px'
+      },
+
+      isRoot: window.location.toString() === 'http://localhost:8080/#/' ||
+              window.location.toString() === 'http://localhost:8080/',
+    }
+  },
+
+
+};
 </script>
